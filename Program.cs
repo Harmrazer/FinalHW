7ede1b29eb836dd860d594f1aa7fad0902a1e48e
Harmrazer <harmrazer@gmail.com>
2022-08-08 17:06:37 +0300

    Code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//**Задача**: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+Console.WriteLine();
+Console.WriteLine("Final HomeWork");
+Console.WriteLine();
+
+
+string [] words = {"Do", "No", "Make", "your", "job", "well", "!!!"};
+
+string[] LongWords(string[] array)
+{   
+     
+    for (int i = 0; i < array.Length; i++)
+        if(array[i].Length >= 3) Console.Write(array[i] + " ");
+        return array;
+}
+
+LongWords(words);
+Console.WriteLine();
